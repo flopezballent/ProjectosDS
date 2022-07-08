87e255c94f0afc0190f0b9f2de743f1b13fb5655
flopezballent <64671460+flopezballent@users.noreply.github.com>
2022-07-08 20:26:53 -0300

    modificando problemas

--- search_sns.py
+++ search_sns.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import os
 
 initial_date = '2022-05-29'
@@ -6,15 +5,4 @@ final_date = '2022-06-25'
 keywords = 'lightyear'
 
 # Using OS library to call CLI commands in Python
-os.system(f'snscrape --jsonl --max-results 500 --since {initial_date} twitter-search \"{keywords} until:{final_date}\" > {keywords}_sns.json')
-=======
-import os
-
-initial_date = '2022-05-29'
-final_date = '2022-06-25'
-keywords = 'lightyear'
-
-# Using OS library to call CLI commands in Python
-os.system(f'snscrape --jsonl --max-results 500 --since {initial_date} twitter-search \"{keywords} until:{final_date}\" > {keywords}_sns.json')
-
->>>>>>> 01dfa172072e1852cec24e52872916c7c47f9f89
+os.system(f'snscrape --jsonl --max-results 500 --since {initial_date} twitter-search \"{keywords} until:{final_date}\" > {keywords}_sns.json'
